7e5ca3b8f1fb8b35af8cdadabccac55f8c23df33
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-15 19:31:43 -0300

    q

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 class Pessoa {
-    nome: string = "Ana";
-    idade: number = 22;
-    
-    imprimir(){
-        console.log( this.nome, this.idade);
-    }
+  nome: string = "Ana";
+  idade: number = 22;
+
+  imprimir() {
+    console.log(`${this.nome} possui ${this.idade} anos`);
+  }
 }
 
 const p = new Pessoa();
-p.imprimir();
 p.nome = "Abner";
+
+const q = new Pessoa();
+q.nome = "Rodrigo";
+
 p.imprimir();
+q.imprimir();
 
